2127369471edf2f21a0754a8290f5db5f79c3dbc
Robin Ricard <ricard.robin@gmail.com>
2016-06-25 20:05:02 +0200

    Use latest flow typed definitions
    https://github.com/flowtype/flow-typed

--- flow/mocha.js
+++
@@ -1,6 +0,0 @@
-/* eslint no-unused-vars: 0 */
-type BlockFn = (done: (err?: Error) => void) => Promise|void;
-declare function describe(description: string, block: BlockFn): void;
-declare function it(description: string, block: BlockFn): void;
-declare function before(block: BlockFn): void;
-declare function after(block: BlockFn): void;
--- flow/rxjs.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-declare module 'rxjs' {
-  declare interface Observer<T> {
-    isUnsubscribed?: boolean;
-    next: (value: T) => void;
-    error: (err: any) => void;
-    complete: () => void;
-  }
-}
